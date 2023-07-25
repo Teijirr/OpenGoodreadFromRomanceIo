e7d9762056546dd98a63f153515f7625fd71fe48
Bastien Andre <32523642+Teijirr@users.noreply.github.com>
2023-07-25 12:30:36 +0200

    Adding main js file

---
+++ Open Goodreads from Romance.io.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Open Goodreads from Romance.io
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.romance.io/books/*
+// @match        https://www.goodreads.com/search?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=romance.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    addGoodreadsSearchLinkOnRomanceIo();
+    autoOpenGoodreadsBookIfOnlyResult();
+})();
+
+function addGoodreadsSearchLinkOnRomanceIo() {
+    if(window.location.hostname.trim() === 'www.romance.io') {
+        const row = document.querySelector('.link-row');
+        const newDiv = document.createElement("div");
+        const a = document.createElement("a");
+        a.classList.add('btn');
+        a.classList.add('btn-red');
+        a.classList.add('bbuy-link');
+        a.style.backgroundColor = "green";
+        a.style.borderColor = "black";
+        a.innerText = 'goodreads'
+        a.target = '_blank'
+        var bookInfo = document.querySelector('.book-info');
+        var title = bookInfo.firstElementChild.firstChild.data.trim();
+        var author = bookInfo.querySelector('.author').firstElementChild.innerText.trim();
+        const url = 'https://www.goodreads.com/search?q=' + title + " " + author + '&fromRomanceIo=true';
+        a.href = url;
+        newDiv.append(a);
+        newDiv.classList.add('buy-buttons');
+        var firstChild = row.firstElementChild;
+        row.insertBefore(newDiv, firstChild);
+    }
+}
+
+function autoOpenGoodreadsBookIfOnlyResult() {
+    if(window.location.hostname.trim() === 'www.goodreads.com') {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const fromRomanceIo = urlParams.get('fromRomanceIo') === 'true';
+        if(fromRomanceIo)
+        {
+            const results = document.querySelector('tbody');
+            const resultCount = results.childElementCount;
+            if(resultCount === 1) {
+                const result = results.firstElementChild;
+                const titlea = result.querySelector('.bookTitle');
+                window.open(titlea.href, '_self');
+            }
+        }
+    }
+}
